53cb2daaa2a26fce0d3ff4d8e13a26bb00ee2bdd
Dominik Figiel <castro992@gmail.com>
2018-06-06 17:01:57 +0200

    Martin (#17)
    * Add files via upload
    
    Changed movieGetAll template. Updated Model for showing the movies, updated Movie and AdminPanel in View.
    
    * Updated Reservation.php controller.
    
    * Updated Movie.php controller.
    
    * Added Type.php to Views
    
    * Modified Reservation.php controller.

--- src/Controllers/Movie.php
+++ src/Controllers/Movie.php
@@ -3,93 +3,34 @@ namespace Controllers;
 
 class Movie extends Controller {
 
-    public function index()
-    {
-        // Index tworzy obiekt widoku, który zleca wyświetlanie wszystkich produktów w widoku
-        $view = $this->getView('Movie');
-        if(!$view || !$view->index())
-            $this->redirect('error/404.html');
-    }
-    public function getAll(){
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $view= $this->getView('Movie');
-        $data = null;
-        if(\Tools\Sesja::is('message'))
-            $data['message'] = \Tools\Sesja::get('message');
-        if(\Tools\Sesja::is('error'))
-            $data['error'] = \Tools\Sesja::get('error');
-        $view->getAll($data);
-        \Tools\Sesja::clear('message');
-        \Tools\Sesja::clear('error');
-    }
-
-    public function addform(){
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $view = $this->getView('Movie');
-        $view->addform();
-    }
-    public function add()
-    {
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $model = $this->getModel('Movie');
-        $data = $model->add($_POST['Title'], $_POST['ReleaseDate'], $_POST['Age'], $_POST['DurationTime'], $_POST['Cover'], $_POST['Description']);
-        if (isset($data['error']))
-            \Tools\Sesja::set('error', $data['error']);
-        if (isset($data['message']))
-            \Tools\Sesja::set('message', $data['message']);
-        $this->redirect("Film/");
-    }
-    public function delete($id){
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $model = $this->getModel('Movie');
-        $data = $model->delete($id);
-        if(isset($data['error']))
-            \Tools\Sesja::set('error' , $data['error']);
-        if(isset($data['message']))
-            \Tools\Sesja::set('message' , $data['message']);
-        $this->redirect('movie/');
-    }
-
-    public function editform($id){
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $model = $this->getModel('Movie');
-        $data = $model->getOne($id);
-        if(isset($data['error'])){
-            \Tools\Sesja::set('error' , $data['error']);
-            $this->redirect('movie/');
-        }
-        $widok = $this->getView('Movie');
-        $widok->editform($data['movie']);
-    }
-
-    public function update(){
-        $accessController = new \Controllers\Access();
-        $accessController->islogin();
-        $model = $this->getModel('Movie');
-        $data = $model->update($_POST['IdMovie'] , $_POST['Title'] ,$_POST['ReleaseDate'],  $_POST['Age'],  $_POST['DurationTime'],  $_POST['Cover'],  $_POST['Description'] );
-        if(isset($data['error']))
-            \Tools\Session::set('error' , $data['error']);
-        if(isset($data['message']))
-            \Tools\Session::set('message' , $data['message']);
-        $this->redirect('Film/');
-    }
-
     public function getOne($id){
-            \Tools\Session::set("navigation", "Movie");
+        \Tools\Session::set("navigation", "Movie");
+        $view = $this->getView('Movie');
+        $data = null;
+        if (\Tools\Session::is('message'))
+            $data['message'] = \Tools\Session::get('message');
+        if (\Tools\Session::is('error'))
+            $data['error'] = \Tools\Session::get('error');
+        $view->getOne($id, $data);
+        \Tools\Session::clear('message');
+        \Tools\Session::clear('error');
+    }
+
+    public function getAllWithoutShowing(){
+        if(!\Tools\Access::islogin()) {
+            \Tools\Session::set("navigation", "InComing");
             $view = $this->getView('Movie');
             $data = null;
             if (\Tools\Session::is('message'))
                 $data['message'] = \Tools\Session::get('message');
             if (\Tools\Session::is('error'))
                 $data['error'] = \Tools\Session::get('error');
-            $view->getOne($id, $data);
+            $view->getAllWithoutShowing($data);
             \Tools\Session::clear('message');
             \Tools\Session::clear('error');
+        }
+        else
+            $this->redirect('');
     }
 
 
@@ -159,11 +100,16 @@ class Movie extends Controller {
         if(\Tools\Access::islogin()) {
             $model = $this->getModel('Movie');
 
+            $cover = array();
+            $cover['picture'] = $_POST['Cover'];
+            $cover['imageName'] = $_FILES['Cover']['name'];
+            $cover['type'] = $_FILES['Cover']['type'];
+            $cover['imageTemp'] = $_FILES['Cover']['tmp_name'];
             $Genre = $_POST['Genre'];
             $Productions = $_POST['Production'];
             $movie = $model->addMovie($_POST['Title'], $_POST['ReleaseDate'], $_POST['Age'],
-                                        $_POST['DurationTime'], "COVER", $_POST['Description'],
-                                        $Genre, $Productions);
+                $_POST['DurationTime'], $_POST['Description'],
+                $Genre, $Productions, $cover);
             if(isset($movie['message'])){
                 \Tools\Session::set('message', $movie['message']);
             }
@@ -181,20 +127,54 @@ class Movie extends Controller {
         if(\Tools\Access::islogin()) {
             $model = $this->getModel('Movie');
 
+            //Edytujemy okładkę
+            //if(file_exists("resources/images/covers/".$_POST['idMovie'].".jpg"))
+            //rename("resources/images/covers/".$_POST['idMovie'].".jpg", "resources/images/covers/tmp.jpg");
+
             //Najpierw usuwamy
             //$this->deleteMovie($_POST['idMovie']);
-            $model->deleteMovie($_POST['idMovie']);
+            //$model->deleteMovie($_POST['idMovie']);
 
             //Później dodajemy
+            /*$cover = null;
+            if(isset($_FILES['Cover']) && $_FILES['Cover']['name'] != "" &&
+                $_FILES['Cover']['type'] != "" && $_FILES['Cover']['tmp_name'] != "") {
+                $cover = array();
+                $cover['imageName'] = $_FILES['Cover']['name'];
+                $cover['type'] = $_FILES['Cover']['type'];
+                $cover['imageTemp'] = $_FILES['Cover']['tmp_name'];
+                var_dump("Weszło");
+            }
             $Genre = $_POST['Genre'];
             $Productions = $_POST['Production'];
             $movie = $model->addMovie($_POST['Title'], $_POST['ReleaseDate'], $_POST['Age'],
-                $_POST['DurationTime'], "COVER", $_POST['Description'],
-                $Genre, $Productions);
+                $_POST['DurationTime'], $_POST['Description'],
+                $Genre, $Productions, $cover);
             if(isset($movie['message'])){
                 \Tools\Session::set('message', "Udało się edytować film.");
             }
             if(isset($movie['error'])){
+                \Tools\Session::set('error', $movie['error']);
+            }*/
+
+            //EDYCJA
+            $cover = null;
+            if(isset($_FILES['Cover']) && $_FILES['Cover']['name'] != "" &&
+                $_FILES['Cover']['type'] != "" && $_FILES['Cover']['tmp_name'] != "") {
+                $cover = array();
+                $cover['imageName'] = $_FILES['Cover']['name'];
+                $cover['type'] = $_FILES['Cover']['type'];
+                $cover['imageTemp'] = $_FILES['Cover']['tmp_name'];
+                var_dump("Weszło");
+            }
+            $Genre = $_POST['Genre'];
+            $Productions = $_POST['Production'];
+            $movie = $model->editMovie($_POST['idMovie'] , $_POST['Title'], $_POST['ReleaseDate'], $_POST['Age'],
+                $_POST['DurationTime'], $Genre, $Productions, $_POST['Description'], $cover);
+            if(isset($movie['message'])){
+                \Tools\Session::set('message', "Udało się edytować film.");
+            }
+            if(isset($movie['error'])) {
                 \Tools\Session::set('error', $movie['error']);
             }
 
