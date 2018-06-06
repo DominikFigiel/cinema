48dabe481e90266e91acf338e89dd1083d951184
Dominik <castro992@gmail.com>
2018-06-06 17:53:59 +0200

    Updating models

--- src/Models/Production.php
+++ src/Models/Production.php
@@ -2,7 +2,6 @@
 namespace Models;
 use \PDO;
 class Production extends Model {
-
     public function getAll(){
         if($this->pdo === null){
             $data['error'] = \Config\Database\DBErrorName::$connection;
@@ -18,7 +17,6 @@ class Production extends Model {
             $stmt = $this->pdo->query($query);
             $productions = $stmt->fetchAll();
             $stmt->closeCursor();
-
             if($productions && !empty($productions))
                 $data['productions'] = $productions;
         }
@@ -27,7 +25,6 @@ class Production extends Model {
         }
         return $data;
     }
-
     public function addProduction($name){
         if($this->pdo === null){
             $data['error'] = \Config\Database\DBErrorName::$connection;
@@ -59,7 +56,6 @@ class Production extends Model {
         }
         return $data;
     }
-
     public function addProductionForMovie($idMovie, $idProduction){
         if($this->pdo === null){
             $data['error'] = \Config\Database\DBErrorName::$connection;
@@ -72,7 +68,7 @@ class Production extends Model {
         $data = array();
         //var_dump($idProduction);
         //if(!is_numeric($idProduction))
-            //$idProduction = intval($idProduction);
+        //$idProduction = intval($idProduction);
         try{
             $query = '           
                 INSERT INTO '.\Config\Database\DBConfig::$tableMovieProduction.' ('.\Config\Database\DBConfig\MovieProduction::$IdMovie.',
@@ -96,7 +92,6 @@ class Production extends Model {
         }
         return $data;
     }
-
     public function deleteProductionsForMovie($idMovie){
         if($this->pdo === null){
             $data['error'] = \Config\Database\DBErrorName::$connection;
@@ -128,7 +123,6 @@ class Production extends Model {
         }
         return $data;
     }
-
     public function addProductionsForMovie($idMovie, $idProductions){
         $data = array();
         if(is_null($idMovie) || is_null($idProductions)){
@@ -142,7 +136,6 @@ class Production extends Model {
                     $data['error'] = $productionForMovie['error'];
                     return $data;
                 }
-
             }
         }
         else{
@@ -152,5 +145,85 @@ class Production extends Model {
         }
         return $data;
     }
-
+    public function editProductionForMovie($idMovieProduction, $idProduction){
+        $data = array();
+        if($this->pdo === null){
+            $data['error'] = \Config\Database\DBErrorName::$connection;
+            return $data;
+        }
+        if(is_null($idMovieProduction) || is_null($idProduction)){
+            $data['error'] = \Config\Database\DBErrorName::$empty;
+            return $data;
+        }
+        try{
+            $query = '           
+                UPDATE '.\Config\Database\DBConfig::$tableMovieProduction.' 
+                SET '.\Config\Database\DBConfig\MovieProduction::$IdProduction.' = :idProduction
+                WHERE '.\Config\Database\DBConfig::$tableMovieProduction.'.'.\Config\Database\DBConfig\MovieProduction::$IdMovieProduction.' = :idMovieProduction
+            ';
+            $stmt = $this->pdo->prepare($query);
+            $stmt->bindValue(':idMovieProduction' , $idMovieProduction , PDO::PARAM_INT);
+            $stmt->bindValue(':idProduction' , $idProduction , PDO::PARAM_INT);
+            $result = $stmt->execute();
+            if($result === true){
+                $data['message'] = "Udało sie edytować.";
+            }
+            else{
+                $data['error'] = "Nie udało się edytować.";
+            }
+            $stmt->closeCursor();
+        }
+        catch(\PDOException $e){
+            $data['error'] = \Config\Database\DBErrorName::$query;
+        }
+        return $data;
+    }
+    public function editProductionsForMovie($idMovie, $idProductions){
+        $data = array();
+        if(is_null($idMovie) || is_null($idProductions)){
+            $data['error'] = \Config\Database\DBErrorName::$empty;
+            return $data;
+        }
+        $ids = $this->getProductionsMovieIdForMovieId($idMovie);
+        if (is_array($idProductions)){
+            if (count($idProductions) <= count($ids)){
+                foreach ($ids as $id){
+                    $this -> editProductionForMovie($id, $idProductions);
+                }
+            }
+        }
+        elseif (is_numeric($idProductions)){
+        }
+        return $data;
+    }
+    private function getProductionsMovieIdForMovieId($idMovie){
+        $data = array();
+        if($this->pdo === null){
+            $data['error'] = \Config\Database\DBErrorName::$connection;
+            return $data;
+        }
+        if(is_null($idMovie)){
+            $data['error'] = \Config\Database\DBErrorName::$empty;
+            return $data;
+        }
+        try{
+            $query = '           
+                SELECT '.\Config\Database\DBConfig::$tableMovieProduction.'.'.\Config\Database\DBConfig\MovieProduction::$IdMovieProduction.' 
+                WHERE '.\Config\Database\DBConfig::$tableMovieProduction.'.'.\Config\Database\DBConfig\MovieProduction::$IdMovie.' = :idMovie
+            ';
+            $stmt = $this->pdo->prepare($query);
+            $stmt->bindValue(':idMovie' , $idMovie , PDO::PARAM_INT);
+            $result = $stmt->execute();
+            $ids =$stmt->fetchAll();
+            $data['ids'] = array();
+            foreach ($ids as $id){
+                $data['ids'][] = $id[\Config\Database\DBConfig\MovieProduction::$IdProduction];
+            }
+            $stmt->closeCursor();
+        }
+        catch(\PDOException $e){
+            $data['error'] = \Config\Database\DBErrorName::$query;
+        }
+        return $data;
+    }
 }
